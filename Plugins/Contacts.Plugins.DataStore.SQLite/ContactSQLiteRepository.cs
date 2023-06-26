437f2960be1ea267091de0399885251353fc0e37
Fahrizal <fahrizal.fahruddin@gmail.com>
2023-06-26 13:07:39 +0800

    Add SQLite repository

---
+++ Plugins/Contacts.Plugins.DataStore.SQLite/ContactSQLiteRepository.cs
@@ -0,0 +1,59 @@
+﻿using Contacts.UseCases.PluginInterfaces;
+using SQLite;
+using Contact = Contacts.CoreBusiness.Contact;
+
+namespace Contacts.Plugins.DataStore.SQLite
+{
+    public class ContactSQLiteRepository : IContactRepository
+    {
+        private SQLiteAsyncConnection database;
+
+        public ContactSQLiteRepository()
+        {
+            database = new SQLiteAsyncConnection(Constants.DatabasePath);
+            database.CreateTableAsync<Contact>().Wait();
+        }
+
+        public async Task AddContactAsync(Contact contact)
+        {
+            await database.InsertAsync(contact);
+        }
+
+        public async Task DeleteContactAsync(int contactId)
+        {
+            var contact = await GetContactByIdAsync(contactId);
+
+            if (contact != null && contact.ContactId == contactId)
+            {
+                await database.DeleteAsync(contact);
+            }
+        }
+
+        public async Task<Contact> GetContactByIdAsync(int contactId)
+        {
+            return await database.Table<Contact>()
+                .Where(c => c.ContactId == contactId)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Contact>> GetContactsAsync(string filterText)
+        {
+            if (string.IsNullOrWhiteSpace(filterText))
+            {
+                return await database.Table<Contact>().ToListAsync();
+            }
+
+            return await database.QueryAsync<Contact>(@"SELECT * FROM Contact 
+                    WHERE Name LIKE ? OR Email LIKE ? OR Phone LIKE ? OR Address LIKE ?",
+                    $"{filterText}%", $"{filterText}%", $"{filterText}%", $"{filterText}%");
+        }
+
+        public async Task UpdateContactAsync(int contactId, Contact contact)
+        {
+            if (contactId == contact.ContactId)
+            {
+                await database.UpdateAsync(contact);
+            }
+        }
+    }
+}
